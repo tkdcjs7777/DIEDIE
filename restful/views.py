9a56f86a1f3b54868bc4bbb66e3f8661c1b74593
tkcjs7777 <tkdcjs7777@gmail.com>
2018-07-18 00:57:52 +0900

    bug

--- restful/views.py
+++ restful/views.py
@@ -5,7 +5,7 @@ from .models import GameUserInfo, Ranking
 from django.core.exceptions import ObjectDoesNotExist
 import json
 
-@csrf_exempt 
+@csrf_exempt
 def Login(request):
      if request.method == "POST":
          user_id = request.POST['user_id']
@@ -16,24 +16,24 @@ def Login(request):
                  return JsonResponse({'sucess': 'true'})
          else:
              return JsonResponse({'sucess': 'false'})
-                 
-@csrf_exempt 
+
+@csrf_exempt
 def Account(request):
      if request.method == "POST":
          user_id = request.POST['user_id']
          user_password = request.POST['user_password']
-         
+
          try:
             user_obj = GameUserInfo.objects.get(user_id=user_id)
          except ObjectDoesNotExist:
-            return JsonResponse({'sucess': 'false'})
+            print("")
         #유저 중복 가입 방지
          if user_obj:
-             return JsonResponse({'sucess': 'false'}) 
+             return JsonResponse({'sucess': 'false'})
 
          #공백 회원가입 방지
          if user_id == "" or user_password=="" or user_id == None or user_password == None:
-             return JsonResponse({'sucess': 'false'}) 
+             return JsonResponse({'sucess': 'false'})
          GameUserInfo.objects.create(user_id=user_id, user_password=user_password)
          return JsonResponse({'sucess': 'true'})
 
@@ -42,7 +42,7 @@ def GetTotalRank(request):
 
     #클리어 순서대로 가져오기
     rankingObj = Ranking.objects.all().order_by("clear_time")
-    #json은 순서, 아이디, 클리어시간 
+    #json은 순서, 아이디, 클리어시간
     JsonRankData = dict()
     seq = 1
     for obj in rankingObj:
@@ -55,12 +55,12 @@ def GetTotalRank(request):
         seq = seq+1
     print(json.dumps(JsonRankData))
     return JsonResponse(JsonRankData)
-        
+
 @csrf_exempt
 def GetMyRank(request, user_id):
     #유저 아이디 검색후 정렬 데이터
     rankingObj = Ranking.objects.filter(user_id__user_id__exact=user_id).order_by("clear_time")
-    #json 구성요소는 순서, 아이디, 클리어시간 
+    #json 구성요소는 순서, 아이디, 클리어시간
     JsonRankData = dict()
     seq = 1
     for obj in rankingObj:
@@ -79,11 +79,3 @@ def SetRank(request, user_id, clear_time):
     user_obj = GameUserInfo.objects.get(user_id=user_id)
     Ranking.objects.create(user_id=user_obj,clear_time=float(clear_time))
     return JsonResponse({'sucess': 'true'})
-    
-
-    
-
-
-
-
-    
